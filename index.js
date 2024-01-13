34df809659b66ba54c36fd027893dd2a54f079c9
gujjaripavan <pavan.gujjari09@gmail.com>
2024-01-13 23:01:43 +0530

    Simple RESTful initiated.

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoURI = process.env.MONGODB_URI;
+const routes = require("./routes/routes");
+
+mongoose.connect(mongoURI).then(() => console.log("Database Connected..."));
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", routes);
+
+app.listen(5000, () => {
+  console.log(`Server Strted on port 5000`);
+});
